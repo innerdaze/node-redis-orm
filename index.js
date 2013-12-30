a844cec0e12eca423ccd1b235910f6082e19b76c
lee <leed@ibcos.co.uk>
2013-12-30 11:47:31 +0000

    added responses and redis client

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var api = require('./lib/redis-api');
 var model = require('./lib/redis-model');
 
-module.exports = {
-	API: api,
-	Model: model
+module.exports = ORM = function(config){
+	return {
+		API: new api(config),
+		Model: model
+	};
 };
