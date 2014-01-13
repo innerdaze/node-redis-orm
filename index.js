14cdd4bbb25530a712f95e44ebe64ba6d23ab2c7
lee <leed@ibcos.co.uk>
2014-01-13 10:21:58 +0000

    restructured ORM constructor

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var StringSPI = require('./lib/redis-string-spi'),
 		ResourceModel = require('./lib/redis-resource-model'),
 		ORM;
 
-module.exports = ORM = function(config){
-	return {
-		StringSPI: new StringSPI(config),
-		ResourceModel: ResourceModel
-	};
+module.exports = ORM = {
+	StringSPI: StringSPI,
+	ResourceModel: ResourceModel
 };
